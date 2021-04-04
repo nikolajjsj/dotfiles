0992701f7486c3ee6afc44b32151e37a8c4be3cc
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-04-04 15:01:23 +0200

    Ask for sudo

--- macos.sh
+++ macos.sh
@@ -4,6 +4,12 @@
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 #<----------------Changing MacOS Specific Preferences-------------------->
 # Finder
 defaults write com.apple.finder ShowPathbar -bool true #Adds the path bar to the bottom of the finder
