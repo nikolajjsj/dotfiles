9397d6e41d56600b74594cae56a3c74eaecc540d
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-04-04 15:16:52 +0200

    Updated install.sh script

--- install.sh
+++ install.sh
@@ -3,21 +3,18 @@
 echo 'Are the Xcode command line tools installed?'
 echo 'If not exit using CMD+C, and install with:'
 echo 'xcode-select -install'
+read -p "Continue? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	# cloning github repo
+    cd ~
 
-read answer
+    # create directory needed for config.fish (symlink)
+    mkdir ~/.config/fish/
 
-# cloning github repo
-cd ~
-mkdir dev
-cd dev
-git clone https://github.com/nikolajjsj/.dotfiles.git
-cd .dotfiles
-
-# create directory needed for config.fish (symlink)
-mkdir ~/.config/fish/
-
-# symlinking
-ln -sf ~/dev/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dev/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dev/.dotfiles/config.fish ~/.config/fish/config.fish
-ln -sf ~/dev/.dotfiles/alacritty.yml ~/.config/alacritty.yml
+    # symlinking
+    ln -sf ~/dev/.dotfiles/.gitconfig ~/.gitconfig
+    ln -sf ~/dev/.dotfiles/.vimrc ~/.vimrc
+    ln -sf ~/dev/.dotfiles/config.fish ~/.config/fish/config.fish
+    ln -sf ~/dev/.dotfiles/alacritty.yml ~/.config/alacritty.yml
+fi;
