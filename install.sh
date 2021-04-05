2a54e18b1f46a7e2f469997645145d330dae5888
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-04-05 11:22:00 +0200

    Improved the installation process

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 echo 'Are the Xcode command line tools installed?'
+echo 'And have you installed HomeBrew?'
+echo ""
 echo 'If not exit using CMD+C, and install with:'
 echo 'xcode-select -install'
+echo 'and/or:'
+echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+echo ""
 read -p "Continue? (y/n) " -n 1;
 echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	# cloning github repo
     cd ~
 
     # create directory needed for config.fish (symlink)
@@ -18,8 +22,28 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     ln -sf ~/dev/.dotfiles/.vimrc ~/.vimrc
     ln -sf ~/dev/.dotfiles/config.fish ~/.config/fish/config.fish
     ln -sf ~/dev/.dotfiles/alacritty.yml ~/.config/alacritty.yml
+
+    # Prompt user if they want to set MacOS preferences
+    read -p "Set MacOS settings? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sh ./macos.sh
+    fi;
+
+    # Prompt user if they want to install HomeBrew packages
+    read -p "Install HomeBrew packages? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sh ./brew.sh
+    fi;
+
+    # Prompt user if they want to set fish as the default shell
+    read -p "Set fish as default shell? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      # Change the default shell to fish
+      echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+      chsh -s /usr/local/bin/fish
+    fi;
 fi;
 
-# Change the default shell to fish
-# echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-# chsh -s /usr/local/bin/fish
