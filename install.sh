5e360ba2bfe673b6386e81f6356f0460829fec6c
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-08-07 17:01:51 +0200

    Add symlinking for .bashrc

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     # symlinking
     ln -sf ~/code/.dotfiles/.gitconfig ~/.gitconfig
     ln -sf ~/code/.dotfiles/.zshrc ~/.zshrc
+    ln -sf ~/code/.dotfiles/.bashrc ~/.bashrc
     ln -s ~/code/.dotfiles/nvim ~/.config/
     ln -sf ~/code/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
     ln -sf ~/code/.dotfiles/.tmux.conf ~/.tmux.conf
