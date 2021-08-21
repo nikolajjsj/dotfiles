c8db746d625e578ba2c66a2e1d206abf57d405ba
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-08-21 19:44:33 +0200

    update lua configs (statusbar and telescope)

--- nvim/lua/telescopes.lua
+++ nvim/lua/telescopes.lua
@@ -43,3 +43,7 @@ require('telescope').setup{
   }
 }
 
+vim.api.nvim_set_keymap('n', '<Leader>ff', '<Cmd>Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fg', '<Cmd>Telescope live_grep<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fb', '<Cmd>Telescope buffers<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fh', '<Cmd>Telescope help_tags<CR>', { noremap = true })
