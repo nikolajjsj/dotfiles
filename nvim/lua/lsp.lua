fc0ddcc0a17c22c0a43211d87fa34d55eec01a80
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-08-21 19:45:02 +0200

    Updated lua config for lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -20,7 +20,13 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   --buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-
+  
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_command [[ augroup Format ]]
+    vim.api.nvim_command [[ autocmd! * <buffer> ]]
+    vim.api.nvim_command [[ autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync() ]]
+    vim.api.nvim_command [[ augroup END ]]
+  end
 end
 
 nvim_lsp.tsserver.setup {
