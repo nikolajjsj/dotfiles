94d2ee5f2cefc6952317d246b53769d9a23a0119
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-09-01 19:47:42 +0200

    Fix autopair configuration

--- nvim/lua/autopair.lua
+++ nvim/lua/autopair.lua
@@ -5,8 +5,8 @@ autopairs.setup({
   disable_filetype = { "TelescopePrompt", "vim" }
 })
 
-require('nvim-autopairs.completion.compe').setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true -- it will auto insert `(` after select function or method item
-})
+-- require('nvim-autopairs.completion.cmp').setup({
+--   map_cr = true, --  map <CR> on insert mode
+--   map_complete = true -- it will auto insert `(` after select function or method item
+-- })
 
