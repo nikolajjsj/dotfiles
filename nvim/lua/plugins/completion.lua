a714d24c4223f54179c3d3b45d5aab0fb0d581ab
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-10-09 21:25:54 +0000

    Cleanup init.lua + update cmp configuration

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,17 +1,13 @@
 local cmp = require'cmp'
+local autopair = require'nvim-autopairs.completion.cmp'
 
 cmp.setup({
   mapping = {
-    ['K'] = cmp.mapping.scroll_docs(-4),
-    ['J'] = cmp.mapping.scroll_docs(4),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+      ['K'] = cmp.mapping.scroll_docs(-4),
+      ['J'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
   sources = {
     { name = 'nvim_lsp'},
@@ -19,8 +15,13 @@ cmp.setup({
   },
 })
 
-require("nvim-autopairs.completion.cmp").setup({
+autopair.setup({
   map_cr = true, --  map <CR> on insert mode
   map_complete = true, -- it will auto insert `(` after select function or method item
-  auto_select = true -- automatically select the first item
+  auto_select = true, -- automatically select the first item
+  insert = false, -- use insert confirm behavior instead of replace
+  map_char = { -- modifies the function or method delimiter by filetypes
+    all = '(',
+    tex = '{'
+  }
 })
