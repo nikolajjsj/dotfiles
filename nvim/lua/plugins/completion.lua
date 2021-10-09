e6cab8c063621e8accfca4480090f3d50854f694
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-10-09 21:31:55 +0000

    Fix bug with autopairs plugin not being called

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,5 +1,4 @@
 local cmp = require'cmp'
-local autopair = require'nvim-autopairs.completion.cmp'
 
 cmp.setup({
   mapping = {
@@ -15,7 +14,8 @@ cmp.setup({
   },
 })
 
-autopair.setup({
+require('nvim-autopairs').setup()
+require("nvim-autopairs.completion.cmp").setup({
   map_cr = true, --  map <CR> on insert mode
   map_complete = true, -- it will auto insert `(` after select function or method item
   auto_select = true, -- automatically select the first item
