271890aca8327792731642052c0d4cff3979860b
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-10-13 18:26:13 +0200

    Removed some code that was not needed in the neovim completion configuration

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -15,8 +15,6 @@ cmp.setup({
   mapping = {
     ['K'] = cmp.mapping.scroll_docs(-4),
     ['J'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
