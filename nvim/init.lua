85d1c772d19f942135d91e39de991e8d83233e0b
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-03 16:38:52 +0100

    Put mapping and theme configs in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,44 @@
 local api = vim.api
 local g = vim.g
 local opt = vim.opt
+local map = api.nvim_set_keymap
 
 --Remap space as leader key
-api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
 g.mapleader = ' '
+-- Disable builtins
+g.loaded_gzip = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
 
+-- Quickly edit and source neovim config
+map('n', '<leader>ve', ':edit ~/code/.dotfiles/nvim/init.lua<cr>', { noremap = true })
+map('n', '<leader>vr', ':source ~/code/.dotfiles/nvim/init.lua<cr>', { noremap = true })
+-- Quick switch between two last opened buffer
+map('n', '<leader><leader>', '<c-^>', { noremap = true})
+-- Line navigation
+map('n', 'H', '^', {})
+map('n', 'L', '$', {})
+-- Navigation between windows
+map('', '<leader>h', ':wincmd h<CR>', {})
+map('', '<leader>j', ':wincmd j<CR>', {})
+map('', '<leader>k', ':wincmd k<CR>', {})
+map('', '<leader>l', ':wincmd l<CR>', {})
+-- Split window
+map('n', 'ss', ':split<Return><C-w>w', {})
+map('n', 'sv', ':vsplit<Return><C-w>w', {})
+-- Open Terminal in split window
+map('n', '<Leader>t', ':below 10sp term://$SHELL<CR>i', { noremap = true })
+-- Better Visual lines and handling
+map("v", "J", ":m '>+1<CR>gv=gv", { noremap = true })
+map("v", "K", ":m '<-2<CR>gv=gv", { noremap = true })
+map('v', '<Tab>', '>gv', { noremap = true })
+map('v', '<S-Tab>', '<gv', { noremap = true })
+map('v', '<', '<gv', {})
+map('v', '>', '>gv', {})
+
+-- Setting options
 opt.hidden = true
 opt.colorcolumn = '80'
 opt.signcolumn = 'yes'
@@ -35,16 +68,14 @@ opt.updatetime = 1000 -- Make updates happen faster
 opt.lazyredraw = true
 opt.scrolloff = 10 -- Make it so there are always ten lines below my cursor
 
--- Disable builtins
-g.loaded_gzip = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
+-- Theming
+vim.o.background = 'dark'
+vim.g.gruvbox_contrast_dark = 'hard'
+vim.g.gruvbox_invert_selection = '0'
+vim.cmd([[colorscheme gruvbox]])
 
--- Sourcing plugins and mappin
+-- Sourcing plugins and mappings
 require('plugins')
-require('mappings')
 require('theme')
 
 -- Lua plugins configurations
--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,5 +1,1 @@
-vim.o.background = 'dark'
-vim.g.gruvbox_contrast_dark = 'hard'
-vim.g.gruvbox_invert_selection = '0'
-vim.cmd([[colorscheme gruvbox]])
 
