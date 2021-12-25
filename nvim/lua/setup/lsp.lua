251a89a14e45705413baaa7f7b8e52af04aab337
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-25 10:15:18 +0100

    feature: Use rust-analyzer instead of rls

--- nvim/lua/setup/lsp.lua
+++ nvim/lua/setup/lsp.lua
@@ -164,7 +164,18 @@ lspconfig.jsonls.setup {
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'vuels', 'angularls', 'bashls', 'cssls', 'html', 'dartls', 'sqlls', 'rls'}
+local servers = { 
+  'pyright', 
+  'vuels', 
+  'angularls', 
+  'bashls', 
+  'cssls', 
+  'html', 
+  'dartls', 
+  'sqlls', 
+  -- 'rls',
+  'rust_analyzer',
+}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
