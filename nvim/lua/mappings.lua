b14f098a66c777a3ab525f96c40d83974ee78bfc
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-01-10 19:47:39 +0100

    feature: Update window navigation keymaps

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,27 +1,29 @@
 local map = vim.api.nvim_set_keymap
 
+local opts = { noremap = true }
+
 -- Quickly edit and source neovim config
-map('n', '<leader>ve', ':edit ~/code/.dotfiles/nvim/init.lua<cr>', { noremap = true })
-map('n', '<leader>vr', ':source ~/code/.dotfiles/nvim/init.lua<cr>', { noremap = true })
+map('n', '<leader>ve', ':edit ~/code/.dotfiles/nvim/init.lua<cr>', opts)
+map('n', '<leader>vr', ':source ~/code/.dotfiles/nvim/init.lua<cr>', opts)
 -- Quick switch between two last opened buffer
-map('n', '<leader><leader>', '<c-^>', { noremap = true})
+map('n', '<leader><leader>', '<c-^>', opts)
 -- Line navigation
-map('n', 'H', '^', {})
-map('n', 'L', '$', {})
+map('n', 'H', '^', opts)
+map('n', 'L', '$', opts)
 -- Navigation between windows
-map('', '<leader>h', ':wincmd h<CR>', {})
-map('', '<leader>j', ':wincmd j<CR>', {})
-map('', '<leader>k', ':wincmd k<CR>', {})
-map('', '<leader>l', ':wincmd l<CR>', {})
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
 -- Split window
-map('n', 'ss', ':split<Return><C-w>w', {})
-map('n', 'sv', ':vsplit<Return><C-w>w', {})
+map('n', 'ss', ':split<Return><C-w>w', opts)
+map('n', 'sv', ':vsplit<Return><C-w>w', opts)
 -- Open Terminal in split window
-map('n', '<Leader>t', ':below 10sp term://$SHELL<CR>i', { noremap = true })
+map('n', '<Leader>t', ':below 10sp term://$SHELL<CR>i', opts)
 -- Better Visual lines and handling
-map("v", "J", ":m '>+1<CR>gv=gv", { noremap = true })
-map("v", "K", ":m '<-2<CR>gv=gv", { noremap = true })
-map('v', '<Tab>', '>gv', { noremap = true })
-map('v', '<S-Tab>', '<gv', { noremap = true })
-map('v', '<', '<gv', {})
-map('v', '>', '>gv', {})
+map("v", "J", ":m '>+1<CR>gv=gv", opts)
+map("v", "K", ":m '<-2<CR>gv=gv", opts)
+map('v', '<Tab>', '>gv', opts)
+map('v', '<S-Tab>', '<gv', opts)
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
