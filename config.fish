9635a153336cc1afd12648737f62face76bfe0ce
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-01-20 14:07:23 +0100

    feature: Add starship prompt to config.fish

--- config.fish
+++ config.fish
@@ -16,21 +16,5 @@ alias fzf="fzf --height=80% --preview 'bat --style=numbers --color=always {}'"
 # Recursively clean .DS_Store files
 alias cleanup='find . -type f -name "*.DS_Store" -ls -delete'
 
-# Fish prompt
-function fish_prompt
-	set_color brblack
-	echo -n "["(date "+%H:%M")"] "
-	set_color blue
-	echo -n (hostname)
-	if [ $PWD != $HOME ]
-		set_color brblack
-		echo -n ':'
-		set_color yellow
-		echo -n (basename $PWD)
-	end
-	set_color green
-	printf '%s ' (__fish_git_prompt)
-	set_color red
-	echo -n '| '
-	set_color normal
-end
+# Starship prompt
+starship init fish | source
