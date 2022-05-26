71692ce19964a4049453a0f37f4e91d453e85142
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-05-26 15:16:34 +0200

    refactor: Seperate functionality + add go plugin

---
+++ nvim/lua/options.lua
@@ -0,0 +1,36 @@
+vim.o.number  =true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.expandtab = true
+vim.o.incsearch = true
+vim.o.tabstop = 2
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.hlsearch = false
+vim.o.swapfile = false
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.scrolloff = 10
+vim.o.shiftwidth = 2
+vim.o.numberwidth = 4
+vim.o.termguicolors = true
+vim.o.colorcolumn = '80'
+vim.o.showtabline = 2
+vim.o.signcolumn = 'yes'
+vim.o.mouse = 'a'
+vim.o.background = 'dark'
+vim.o.updatetime = 300 -- faster completion
+vim.o.lazyredraw = true
+vim.o.wildignore = [[
+.git,.hg,.svn
+*.aux,*.out,*.toc
+*.o,*.obj,*.exe,*.dll,*.manifest,*.rbc,*.class
+*.ai,*.bmp,*.gif,*.ico,*.jpg,*.jpeg,*.png,*.psd,*.webp
+*.avi,*.divx,*.mp4,*.webm,*.mov,*.m2ts,*.mkv,*.vob,*.mpg,*.mpeg
+*.mp3,*.oga,*.ogg,*.wav,*.flac
+*.eot,*.otf,*.ttf,*.woff
+*.doc,*.pdf,*.cbr,*.cbz
+*.zip,*.tar.gz,*.tar.bz2,*.rar,*.tar.xz,*.kgb
+*.swp,.lock,.DS_Store,._*
+*/tmp/*,*.so,*.swp,*.zip,**/node_modules/**,**/target/**,**.terraform/**"
+]]
---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,31 @@
+require("lualine").setup({
+  options = {
+    icons_enabled = true,
+    section_separators = {'', ''},
+    component_separators = {'', ''},
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {{
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    }},
+    lualine_x = {
+      { 'diagnostics', sources = {"nvim_diagnostic"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
+      'encoding',
+      'filetype'
+    },
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_c = {{
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    }},
+    lualine_x = {'location'},
+  },
+})
