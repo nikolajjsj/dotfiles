ba02533ac059b8caf228b2722c2764c875e32337
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-07-26 20:48:10 +0200

    feature: add better lsp configuration and support for GO

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,75 +4,81 @@ local path = util.path
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-local servers = {
-	"bashls",
-	"dockerls",
-	"tsserver",
-	"yamlls",
-	"jsonls",
-  "gopls",
-}
--- Use a loop to conveniently call 'setup' on multiple servers
-for _, lsp in ipairs(servers) do
-	nvim_lsp[lsp].setup({
-		on_attach = function(client)
-			-- disable formatting for LSP clients as this is handled by null-ls
-			client.server_capabilities.document_formatting = false
-			client.server_capabilities.document_range_formatting = false
-		end,
-		capabilities = capabilities,
-		settings = {
-			json = {
-				format = { enabled = false },
-				schemas = {
-					{
-						description = "ESLint config",
-						fileMatch = { ".eslintrc.json", ".eslintrc" },
-						url = "http://json.schemastore.org/eslintrc",
-					},
-					{
-						description = "Package config",
-						fileMatch = { "package.json" },
-						url = "https://json.schemastore.org/package",
-					},
-					{
-						description = "Packer config",
-						fileMatch = { "packer.json" },
-						url = "https://json.schemastore.org/packer",
-					},
-					{
-						description = "OpenApi config",
-						fileMatch = { "*api*.json" },
-						url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json",
-					},
-				},
-			},
-			yaml = {
-				schemaStore = {
-					enable = true,
-					url = "https://www.schemastore.org/api/json/catalog.json",
-				},
-				schemas = {
-					kubernetes = "*.yaml",
-					["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-					["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-					["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-					["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-					["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-					["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-					["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-					["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-					["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-					["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-					["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-					["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-				},
-				format = { enabled = false },
-				validate = false, -- TODO: conflicts between Kubernetes resources and kustomization.yaml
-				completion = true,
-				hover = true,
-			},
-		},
-		flags = { debounce_text_changes = 150 },
-	})
+-- Use an on_attach function to only map the following keys 
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- formatting
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_command [[augroup Format]]
+    vim.api.nvim_command [[autocmd! * <buffer>]]
+    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    vim.api.nvim_command [[augroup END]]
+  end
+
+  --protocol.SymbolKind = { }
+  vim.lsp.protocol.CompletionItemKind = {
+    '', -- Text
+    '', -- Method
+    '', -- Function
+    '', -- Constructor
+    '', -- Field
+    '', -- Variable
+    '', -- Class
+    'ﰮ', -- Interface
+    '', -- Module
+    '', -- Property
+    '', -- Unit
+    '', -- Value
+    '', -- Enum
+    '', -- Keyword
+    '﬌', -- Snippet
+    '', -- Color
+    '', -- File
+    '', -- Reference
+    '', -- Folder
+    '', -- EnumMember
+    '', -- Constant
+    '', -- Struct
+    '', -- Event
+    'ﬦ', -- Operator
+    '', -- TypeParameter
+  }
 end
+
+require'lspconfig'.gopls.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls", "serve"},
+  filetypes = {"go", "gomod"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
+}
+
+require'lspconfig'.tsserver.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- icon
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    -- This sets the spacing and the prefix, obviously.
+    virtual_text = {
+      spacing = 4,
+      prefix = ''
+    }
+  }
+)
