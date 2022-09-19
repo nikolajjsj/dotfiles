30886eaa39d000ec1f0fb08879507fcc167d1d89
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-09-19 11:05:17 +0200

    refactor(neovim): disabled null-ls for eslint for now

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -3,7 +3,7 @@ if (not status) then return end
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.stylua,
   }
 })
