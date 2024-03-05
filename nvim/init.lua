a34cfb04b425c22c473c6611a3e8fe6c661909cd
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2024-03-05 20:09:08 +0100

    feat(nvim): change theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -84,12 +84,12 @@ require("lazy").setup({
 
 	-- Themes
 	{
-		"rebelot/kanagawa.nvim",
-		name = "kanagawa",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			vim.cmd.colorscheme("kanagawa")
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }, {})
