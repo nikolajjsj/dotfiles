018b09f9e09ef4577d51ab7b15aa05f9ad296b44
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2024-05-03 08:25:24 +0200

    feat(nix): add initial configuration for nix

--- nixi/flake.nix
+++ nixi/flake.nix
@@ -19,6 +19,67 @@
         system = "aarch64-darwin";
       };
       modules = [
+        ({ pkgs, ... }: {
+          services.nix-daemon.enable = true;
+
+          # Here go the darwin preferences and configuration
+          programs.zsh.enable = true;
+          environment.shells = [ pkgs.bash pkgs.zsh ];
+          environment.loginShell = pkgs.zsh;
+          nix.extraOptions = ''
+            experimental-features = nix-command flakes
+          '';
+          systemPackages = [ pkgs.coreutils ];
+          # Key remapping
+          system.keyboard.enableKeyMapping = true;
+          system.keyboard.remapCapsLockToEscape = true;
+          # Fonts
+          fonts.fontDir.enable = true; # DANGER
+          fonts.fonts = [ 
+            (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+          ];
+
+          # Preferences
+          system.defaults.finder.AppleShowAllExtensions = true;
+          system.defaults.dock.autohide = true;
+          system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+          system.defaults.NGGlobalDomain.KeyRepeat = 1;
+          system.defaults.NGGlobalDomain.InitialKeyRepeat = 14;
+        })
+
+        (inputs.home-manager.darwinModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPkgs = true;
+            users.nikolaj.imports = [
+              ({ pkgs, ... }: {
+                home.stateVersion = "22.11";
+                # Specify the home-manager configuration
+                home.packages = [
+                  pkgs.ripgrep
+                  pkgs.fd
+                  pkgs.bat
+                  pkgs.fzf
+                  pkgs.git
+                  pkgs.exa
+                ];
+                home.sessionVariables = {
+                  EDITOR = "nvim";
+                };
+                programs.zsh.enable = true;
+                programs.zsh.enableCompletion = true;
+                programs.zsh.enableAutosuggestions = true;
+                programs.zsh.enableSyntaxHighlighting = true;
+                programs.kitty = {
+                  enable = true;
+                  enableNerdFonts = true;
+                  settings.font = "FiraCode Nerd Font";
+                  settings.font.size = 14;
+                };
+              })
+            ];
+          };
+        })
       ];
     };
 
