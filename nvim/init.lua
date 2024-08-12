4f9a53813b6b06294521f93057711cb062764761
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2024-08-12 08:43:00 +0200

    fix(nvim): update 'conform.nvim' config

--- nvim/init.lua
+++ nvim/init.lua
@@ -609,8 +609,8 @@ require("lazy").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				go = { "goimports", "gofmt" },
-				javascript = { "biome", { "prettierd", "prettier" } },
-				typescript = { "biome", { "prettierd", "prettier" } },
+				javascript = { "prettierd", "prettier", "biome", stop_after_first = true },
+				typescript = { "prettierd", "prettier", "biome", stop_after_first = true },
 			},
 		},
 	},
